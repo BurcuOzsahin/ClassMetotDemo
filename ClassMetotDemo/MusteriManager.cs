854a9dc61cc33f84c6c7b0eb3c80f6676d0698aa
BURCU ÖZŞAHİN <15050111026@ybu.edu.tr>
2021-01-17 21:51:14 +0300

    Ödev 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public static void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" "+ musteri.Soyad+ " sisteme eklenmiştir.");
+         
+        }
+
+        public static void MusteriListeleme(Musteri[] musteriListesi)
+        {
+            foreach (Musteri musteri in musteriListesi)
+            {
+                Console.WriteLine("Id: "+musteri.Id + ", Müşteri Adı: " + musteri.Ad+ ", Müşteri Soyadı: "+musteri.Soyad);
+
+            }
+
+        }
+
+        public static void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Id bilgisi " + musteri.Id + " olan " + musteri.Ad + " " + musteri.Soyad + " sistemden silinmiştir.");
+
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri() {
+                Id=5,
+                Ad="Burcu",
+              Soyad="Özşahin",
+            };
+            Musteri musteri2 = new Musteri()
+            {
+                Id = 18,
+                Ad = "Engin",
+                Soyad = "Demiroğ",
+            };
+            Musteri musteri3 = new Musteri()
+            {
+                Id = 20,
+                Ad = "Berkay",
+                Soyad = "Bilgin",
+            };
+            Musteri[] musteriListe = new Musteri[] {musteri1,musteri2,musteri3};
+
+            Console.WriteLine("**********************************************************");
+            MusteriManager.MusteriEkleme(musteri1);
+            MusteriManager.MusteriEkleme(musteri2);
+            MusteriManager.MusteriEkleme(musteri3);
+            Console.WriteLine("**********************************************************");
+            MusteriManager.MusteriListeleme(musteriListe);
+            Console.WriteLine("**********************************************************");
+            MusteriManager.MusteriSilme(musteri1);
+            MusteriManager.MusteriSilme(musteri2);
+            MusteriManager.MusteriSilme(musteri3);
+            Console.WriteLine("**********************************************************");
+
+
+
+        }
+      
+    }
+}
